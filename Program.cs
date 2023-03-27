a5c8e818384b13bebcbfd1305a93aee4797e0e95
André Nascimento <andre4.20nascimento@outlook.com>
2023-03-26 22:00:30 -0300

    Troco Calculado

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("--- Calcule o Troco ---\n");
+
+Console.Write("Digite o Valor da Compra..: R$ ");
+decimal valor1 = Convert.ToDecimal(Console.ReadLine());
+
+
+Console.Write($"\nDigite o Valor Pago..: R$ ");
+decimal valor2 = Convert.ToDecimal(Console.ReadLine());
+
+
+decimal troco = valor2 - valor1;
+
+Console.WriteLine($"\nDevolva o Troco de: {troco:c}");
